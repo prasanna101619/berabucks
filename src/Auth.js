7ea8841a81007a901949a13905c53b5dd17aa7aa
Prasanna <pransannabhat619@gmail.com>
2024-09-14 15:42:14 +0530

    commit

--- src/Auth.js
+++ src/Auth.js
@@ -1,41 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Auth.css';
 
 const Auth = () => {
-    const [view, setView] = useState('register');
-
-    const showView = (viewToShow) => {
-        setView(viewToShow);
-    };
-
     return (
         <div id="modal">
             {/* Registration View */}
-            <div className={`view ${view === 'register' ? 'active' : ''}`} id="register-view">
-                <h2>Register</h2>
+            <div className="view active" id="register-view">
+                <h2>&#x1F43B; Bera Let's Go !!! &#x1F680;</h2>
                 <center>
                     <span className="warning-icon">&#9888;</span>
-                    <i className="warning-text">Give your original username <br /> otherwise you may lose the airdrop</i>
+                    <i className="warning-text"> Give your original username <br /> otherwise you may lose the airdrop</i>
                 </center>
                 <br /><br />
                 <input type="text" id="register-username" placeholder="Telegram username" />
                 <input type="password" id="register-password" placeholder="Code sent on the bot" />
                 <input type="password" id="register-repassword" placeholder="Referral code" />
-                <button id="register-button">Register</button>
-                <div className="switch-view">
-                    Already have an account? <a href="#" onClick={(e) => { e.preventDefault(); showView('login'); }}>Login</a>
-                </div>
-            </div>
-
-            {/* Login View */}
-            <div className={`view ${view === 'login' ? 'active' : ''}`} id="login-view">
-                <h2>Login</h2>
-                <input type="text" id="login-username" placeholder="Telegram username" />
-                <input type="password" id="login-password" placeholder="Password" />
-                <button id="login-button">Login</button>
-                <div className="switch-view">
-                    Not registered yet? <a href="#" onClick={(e) => { e.preventDefault(); showView('register'); }}>Register</a>
-                </div>
+                <button id="register-button">Let Me In !</button>
             </div>
         </div>
     );
