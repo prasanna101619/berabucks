9daf6907e7fa509d25bd93121fd287f4cd1d8e54
Prasanna <pransannabhat619@gmail.com>
2024-09-16 02:57:47 +0530

    commit

--- src/Flash.js
+++ src/Flash.js
@@ -1,21 +1,21 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import flashs from './assets/beraflashs.gif';
+import flashs from './assets/beraflash.gif';
 import './Flash.css';
 
 const Flash = () => {
   const navigate = useNavigate();
 
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//         if(localStorage.getItem('userNum')){
-//             navigate('/Home');
-//         }
-//         else{
-//             navigate('/Auth');}
-//     }, 3000);
-//     return () => clearTimeout(timer);
-//   }, [navigate]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        if(localStorage.getItem('userNum')){
+            navigate('/Home');
+        }
+        else{
+            navigate('/Auth');}
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <div style={{backgroundImage:`url(${flashs})`,backgroundRepeat:'no-repeat',backgroundSize:'cover', height:'100vh', width:'100vw',display:'flex',alignItems:'center',justifyContent:'center'}}>
