81d6baa978764e46ed6ddbb699b3bb4167eb0ab5
Prasanna <pransannabhat619@gmail.com>
2024-09-16 02:12:46 +0530

    commit

--- src/Flash.js
+++ src/Flash.js
@@ -1,24 +1,31 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import flashs from './assets/FlashScreen.gif';
+import flashs from './assets/flash.jpg';
+import './Flash.css';
+import RingLoader from "react-spinners/RingLoader";
 
+const flashProps = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 const Flash = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-        if(localStorage.getItem('userNum')){
-            navigate('/Home');
-        }
-        else{
-            navigate('/Auth');}
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, [navigate]);
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//         if(localStorage.getItem('userNum')){
+//             navigate('/Home');
+//         }
+//         else{
+//             navigate('/Auth');}
+//     }, 3000);
+//     return () => clearTimeout(timer);
+//   }, [navigate]);
 
   return (
-    <div style={{height:'100vh', width:'100vw',display:'flex',alignItems:'center',justifyContent:'center'}}>
-      <img src={flashs} />
+    <div style={{backgroundImage:`url(${flashs})`,backgroundRepeat:'no-repeat',backgroundSize:'cover', height:'100vh', width:'100vw',display:'flex',alignItems:'center',justifyContent:'center'}}>
+      <RingLoader color="#00ffff" />
     </div>
   );
 };
