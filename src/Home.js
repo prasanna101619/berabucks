ab24df2199229f226031a7226f5032dfe8fdee1e
Prasanna <pransannabhat619@gmail.com>
2024-09-15 23:03:55 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -358,6 +358,13 @@ function GameComponent() {
                 <button className="leaderboard-button" onClick={toggleLeaderboard}>
                   Leaderboard
                 </button>
+                <button className="leaderboard-button" onClick={()=>{
+        localStorage.removeItem('userNum');
+        localStorage.removeItem('userName');
+        localStorage.removeItem('userId');
+        window.location.reload();
+
+      }}>Clear data from localStorage</button>
               </div>
             )}
           </div>
