0fe28ee7f5003b214986c25b27faa7d76e6c3a02
Prasanna <pransannabhat619@gmail.com>
2024-09-16 02:52:57 +0530

    commit

--- src/Flash.js
+++ src/Flash.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import flashs from './assets/beraflash.gif';
+import flashs from './assets/beraflashs.gif';
 import './Flash.css';
 
 const Flash = () => {
