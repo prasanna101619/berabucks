8eb5ae855b5850eb4df0c763eb13a03947b6192e
Prasnna D <68588759+prasanna101619@users.noreply.github.com>
2024-09-16 20:26:21 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,7 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-   
+   apiKey: "AIzaSyC3EPopzoobbwyYlKkq71lQmuouYPjeLVg",
+  authDomain: "berabucks-71ead.firebaseapp.com",
+  projectId: "berabucks-71ead",
+  storageBucket: "berabucks-71ead.appspot.com",
+  messagingSenderId: "626628372457",
+  appId: "1:626628372457:web:4c0cfbecdc7423aede8d78",
+  measurementId: "G-60CTPT9GY0"
 };
 
 
